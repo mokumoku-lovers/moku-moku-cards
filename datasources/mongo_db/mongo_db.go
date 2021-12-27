d5f22a358176a5f8343c05976bd1fa856220d6fd
xyzhg <xy@xyzhg.com>
2021-12-27 15:54:15 +0900

    #10 feat: MongoDB connection, w/o environment variables.

---
+++ datasources/mongo_db/mongo_db.go
@@ -0,0 +1,37 @@
+package mongo_db
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	//"go.mongo_db.org/mongo_db-driver/mongo_db/readpref"
+)
+
+var DB *mongo.Database
+
+func init() {
+	var err error
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = client.Database("test")
+
+	fmt.Println("Connected to MongoDB!")
+}
