7c8ab151929958cc194c5a3a14be45ca286898f8
xyzhg <xy@xyzhg.com>
2021-12-27 16:01:14 +0900

    #10 feat: url mapping for GET /deck/:deckID

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,8 +1,10 @@
 package app
 
+import "moku-moku-cards/controllers/decks"
+
 func mapUrls() {
 	router.GET("/decks", nil)
-	router.GET("/deck/:deckID", nil)
+	router.GET("/deck/:deckID", decks.GetDeck)
 	router.POST("/deck", nil)
 	router.DELETE("/deck/:deckID", nil)
 	router.PATCH("/deck/:deckID", nil)
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.7
+	go.mongodb.org/mongo-driver v1.8.1
 )
