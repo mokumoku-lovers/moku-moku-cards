4d7a5adca09f70a04eb6a4101348cdc068fe2c2c
Nicole Wert <nicalexxandra@gmail.com>
2022-01-02 13:00:12 -0500

    #10 feat: load mongodb uri as environment variable

--- datasources/mongo_db/mongo_db.go
+++ datasources/mongo_db/mongo_db.go
@@ -3,19 +3,40 @@ package mongo_db
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
 	//"go.mongo_db.org/mongo_db-driver/mongo_db/readpref"
 )
 
-var DB *mongo.Database
+const (
+	mongodbUri = "MONGODB_URI"
+)
+
+var (
+	DB  *mongo.Database
+	uri string
+)
+
+func loadEnvironment() {
+	err := godotenv.Load("./datasources/.env")
+
+	if err != nil {
+		log.Println("Couldn't load environment variables")
+		panic(err.Error())
+	}
+}
 
 func init() {
 	var err error
+	loadEnvironment()
+	uri = os.Getenv(mongodbUri)
 
 	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI(uri)
 
 	// Connect to MongoDB
 	client, err := mongo.Connect(context.TODO(), clientOptions)
