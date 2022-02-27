f86150cf678cba189a53f9bc4223b583b4715c68
JiahuiChen99 <Jiahui1@hotmail.es>
2022-02-27 14:54:59 +0100

    #23 feat: convert string userID to int64

--- controllers/decks/DeckController.go
+++ controllers/decks/DeckController.go
@@ -2,7 +2,9 @@ package decks
 
 import (
 	"moku-moku-cards/services"
+	"moku-moku-cards/utils/errors"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -31,7 +33,12 @@ func GetDecks(c *gin.Context) {
 // GetUserDecks retrieves all the decks from the
 // specified user
 func GetUserDecks(c *gin.Context) {
-	userID := c.Param("userID")
+	userID, err := strconv.ParseInt(c.Param("userID"), 10, 64)
+	if err != nil {
+		badRequest := errors.BadRequest("invalid userID")
+		c.JSON(badRequest.Status, badRequest)
+		return
+	}
 
 	decks, getErr := services.GetUserDecks(userID)
 	if getErr != nil {
