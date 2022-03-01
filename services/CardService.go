f47e72ce42b50064f7d546ae8694eb676d305a21
JiahuiChen99 <Jiahui1@hotmail.es>
2022-03-01 12:59:53 +0100

    #30 refactor: card id is now of type mongo object id

--- services/CardService.go
+++ services/CardService.go
@@ -1,12 +1,17 @@
 package services
 
 import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"moku-moku-cards/domain/cards"
 	"moku-moku-cards/utils/errors"
 )
 
-func GetCard(cardID int64) (*cards.Card, *errors.RestErr) {
-	result := &cards.Card{ID: cardID}
+func GetCard(cardID string) (*cards.Card, *errors.RestErr) {
+	objectID, err := primitive.ObjectIDFromHex(cardID)
+	if err != nil {
+		return nil, errors.BadRequest("invalid card ID")
+	}
+	result := &cards.Card{ID: objectID}
 	if err := result.Get(); err != nil {
 		return nil, err
 	}
@@ -15,12 +20,13 @@ func GetCard(cardID int64) (*cards.Card, *errors.RestErr) {
 
 // PostCard validates the received card information
 // and saves it in the DB
-func PostCard(card cards.Card) *errors.RestErr {
+func PostCard(card *cards.Card) *errors.RestErr {
 	// Validate card information
 	if err := card.ValidateCard(); err != nil {
 		return err
 	}
 
+	card.ID = primitive.NewObjectID()
 	// Save card to the DB
 	if err := card.Post(); err != nil {
 		return err
