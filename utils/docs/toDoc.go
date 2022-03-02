f612c7a9a9f585ab8bd483205e752c7750f69b8c
xyzhg <xy@xyzhg.com>
2022-03-02 12:11:42 +0900

    #28 feat: converting field name to bson key string

--- utils/docs/toDoc.go
+++ utils/docs/toDoc.go
@@ -1,6 +1,9 @@
 package docs
 
-import "go.mongodb.org/mongo-driver/bson"
+import (
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func InterfaceToDoc(v interface{}) (doc *bson.D, err error) {
 	data, err := bson.Marshal(v)
@@ -10,4 +13,27 @@ func InterfaceToDoc(v interface{}) (doc *bson.D, err error) {
 
 	err = bson.Unmarshal(data, &doc)
 	return
+}
+
+func FieldToBson(s string) (name string, err error) {
+	switch s {
+	case "ID":
+		return "_id", nil
+	case "Name":
+		return "name", nil
+	case "Cards":
+		return "cards", nil
+	case "Creator":
+		return "creator", nil
+	case "Date":
+		return "date", nil
+	case "Front":
+		return "front", nil
+	case "Back":
+		return "back", nil
+	case "Image":
+		return "image", nil
+	default:
+		return "", errors.New("failed parsing field name")
+	}
 }
