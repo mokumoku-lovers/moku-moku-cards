8702449a26254acb23da963535701ec24f2ef9c9
Nicole Wert <nicalexxandra@gmail.com>
2022-03-03 16:21:36 -0500

    #29 feat: card FieldToBson

--- utils/docs/toDoc.go
+++ utils/docs/toDoc.go
@@ -1,6 +1,10 @@
 package docs
 
-import "go.mongodb.org/mongo-driver/bson"
+import (
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func InterfaceToDoc(v interface{}) (doc *bson.D, err error) {
 	data, err := bson.Marshal(v)
@@ -10,4 +14,20 @@ func InterfaceToDoc(v interface{}) (doc *bson.D, err error) {
 
 	err = bson.Unmarshal(data, &doc)
 	return
+}
+
+func FieldToBson(s string) (name string, err error) {
+	switch s {
+	case "ID":
+		return "_id", nil
+	case "Front":
+		return "front", nil
+	case "Back":
+		return "back", nil
+	case "Image":
+		return "image", nil
+	default:
+		return "", errors.New("failed parsing field name")
+	}
+
 }
